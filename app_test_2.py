3dc0be2199d4287bed11b22add8fba0d5ff64ecb
Jiazhen Li <jiazhen.li@student.uni-tuebingen.de>
2023-12-08 23:12:18 +0100

    enhanced app

--- app_test_2.py
+++ app_test_2.py
@@ -4,7 +4,6 @@ import streamlit as st
 from streamlit_folium import folium_static
 import osmnx as ox
 import networkx as nx
-import osmapi
 
 # Getting start/dest coordinates
 def get_lat_lon(streetname):
@@ -30,34 +29,17 @@ G = init_osm_graph('Stuttgart')
     
 # Get fastest route from OSM
 def get_osm_route(start_location, dest_location):
-    # convert string address into geographical coordinates
-    # start_coords = get_lat_lon(start_location)
-    # dest_coords = get_lat_lon(dest_location)
-    
-    # G = ox.graph_from_place('Stuttgart', network_type='bike')
-    # G = ox.add_edge_speeds(G)
-    # G = ox.add_edge_travel_times(G)
-    
-    # # Get closest graph nodes to origin and destination
-    # orig_node, destination_node = ox.distance.nearest_nodes(
-    #     G, [start_coords[1], dest_coords[1]], [start_coords[0], dest_coords[0]])
-    
-    # # find shortest path based on travel time
-    # route = nx.shortest_path(G, orig_node, destination_node, weight='travel_time')
-    
+     
     start_data = get_lat_lon(start_location)
     dest_data = get_lat_lon(dest_location)
 
     orig_node = ox.distance.nearest_nodes(G, start_data[1], start_data[0])
-    # find the nearest node to the end location
     dest_node = ox.distance.nearest_nodes(G, dest_data[1], dest_data[0])
-    #  find the shortest path
     shortest_route = nx.shortest_path(G, orig_node, dest_node, weight="length")
     
     return shortest_route
 
-
-    
+   
 # App layout
 APP_TITLE = 'BikeWayFinder'
 APP_SUBTITLE = 'Find the best way by bike from A to B'
@@ -92,19 +74,7 @@ if st.button('Find Route'):
     else:      
         # Get the route based on the selected type
         shortest_route = get_osm_route(start_location, dest_location)
-        
-        # Initialize OSM API
-        # api = osmapi.OsmApi()
-        # # Get the latitude and longitude for each node in the route
-        # coordinates = []
-        # for node_id in route:
-        #     node_info = api.NodeGet(node_id)
-        #     lon = node_info["lon"]
-        #     lat = node_info["lat"]
-        #     coordinates.append((lat, lon))
-
-        # folium.PolyLine(coordinates).add_to(m)
-        
+         
         m = ox.plot_route_folium(G, shortest_route, tiles='openstreetmap')
         folium.Marker(start_data, popup='Start',
                     icon = folium.Icon(color='green', prefix='fa',icon='bicycle')).add_to(m)
