343b085c96a7d3f767a4bdbc60d7937becdb7a70
Jiazhen Li <jiazhen.li@student.uni-tuebingen.de>
2023-12-11 22:08:44 +0100

    updated test.py to match second app test

--- test.py
+++ test.py
@@ -4,8 +4,6 @@ import streamlit as st
 from streamlit_folium import folium_static
 import osmnx as ox
 import networkx as nx
-import osmapi
-
 
 # Getting start/dest coordinates
 def get_lat_lon(streetname):
@@ -21,27 +19,28 @@ def get_lat_lon(streetname):
         # Handle the case where the geocoding service does not return valid data
         return None
  
+# Initializing OSM Graph
+@st.cache_resource
+def init_osm_graph(city):
+    G = ox.graph_from_place(city, network_type='bike')
+    return G
+
+G = init_osm_graph('Stuttgart')
     
 # Get fastest route from OSM
 def get_osm_route(start_location, dest_location):
-    # convert string address into geographical coordinates
-    start_coords = get_lat_lon(start_location)
-    dest_coords = get_lat_lon(dest_location)
-    
-    G = ox.graph_from_place('Stuttgart', network_type='bike')
-    G = ox.add_edge_speeds(G)
-    G = ox.add_edge_travel_times(G)
-    
-    # Get closest graph nodes to origin and destination
-    orig_node, destination_node = ox.distance.nearest_nodes(
-        G, [start_coords[1], dest_coords[1]], [start_coords[0], dest_coords[0]])
-    
-    # find shortest path based on travel time
-    route = nx.shortest_path(G, orig_node, destination_node, weight='travel_time')
-    
-    return route
+     
+    start_data = get_lat_lon(start_location)
+    dest_data = get_lat_lon(dest_location)
 
+    orig_node = ox.distance.nearest_nodes(G, start_data[1], start_data[0])
+    dest_node = ox.distance.nearest_nodes(G, dest_data[1], dest_data[0])
+    shortest_route = nx.shortest_path(G, orig_node, dest_node, weight="length")
+    pathDistance = nx.shortest_path_length(G, orig_node, dest_node, weight='length')
     
+    return shortest_route, pathDistance
+
+   
 # App layout
 APP_TITLE = 'BikeWayFinder'
 APP_SUBTITLE = 'Find the best way by bike from A to B'
@@ -53,7 +52,7 @@ start_location = st.text_input('Enter start location:')
 dest_location = st.text_input('Enter destination:')
 
 # Dropdown for route type
-route_type = st.selectbox('Select route type:', ['Fastest Route', 'Bike-Friendly Route'])
+route_type = st.selectbox('Select route type:', ['Shortest Route', 'Bike-Friendly Route'])
 
 # Button to trigger route calculation
 if st.button('Find Route'):
@@ -64,8 +63,9 @@ if st.button('Find Route'):
     m = folium.Map(location=start_data, zoom_start=11)
 
     # Add markers and polyline
-    folium.Marker(start_data, popup='Start').add_to(m)
-    folium.Marker(dest_data, popup='Destination').add_to(m)
+    folium.Marker(start_data, popup='Start',
+                  icon = folium.Icon(color='green', prefix='fa',icon='bicycle')).add_to(m)
+    folium.Marker(dest_data, popup='Destination', icon = folium.Icon(color='red', icon="flag")).add_to(m)
     
     # Convert route type to lowercase for consistency
     route_type_lower = route_type.lower()
@@ -73,21 +73,15 @@ if st.button('Find Route'):
     if route_type_lower == 'bike-friendly route':
         # Just a straight polyline
         folium.PolyLine([start_data, dest_data]).add_to(m)
-    else:
+    else:      
         # Get the route based on the selected type
-        route = get_osm_route(start_location, dest_location)
-        
-        # Initialize OSM API
-        api = osmapi.OsmApi()
-        # Get the latitude and longitude for each node in the route
-        coordinates = []
-        for node_id in route:
-            node_info = api.NodeGet(node_id)
-            lon = node_info["lon"]
-            lat = node_info["lat"]
-            coordinates.append((lat, lon))
-
-        folium.PolyLine(coordinates).add_to(m)
+        shortest_route, pathDistance = get_osm_route(start_location, dest_location)
+         
+        m = ox.plot_route_folium(G, shortest_route, tiles='openstreetmap')
+        folium.Marker(start_data, popup='Start',
+                      icon = folium.Icon(color='green', prefix='fa',icon='bicycle')).add_to(m)
+        popup_text = f'Destination<br><br>Distance: {round(pathDistance/1000, 1)} km'
+        folium.Marker(dest_data, popup=popup_text, icon = folium.Icon(color='red', icon="flag")).add_to(m)
 
     # Display the map
     folium_static(m, width=700)
