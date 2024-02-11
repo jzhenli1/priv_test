bd4504da6f870bb055a5ec273e2723fad5744e1f
Jiazhen Li <jiazhen.li@student.uni-tuebingen.de>
2024-02-11 17:30:21 +0100

    both weights work, without radio button

--- app_test_2.py
+++ app_test_2.py
@@ -9,6 +9,7 @@ import geopandas as gpd
 from shapely import wkt
 
 # Getting start/dest coordinates
+@st.cache_resource
 def get_lat_lon(streetname):
     BASE_URL = 'https://nominatim.openstreetmap.org/search?format=json'
     response = requests.get(f'{BASE_URL}&street={streetname}&city=Stuttgart')
@@ -40,10 +41,11 @@ def import_data(path):
     merged_osm = merged_osm.set_geometry('geometry')
     return merged_osm
 
-merged_osm = import_data('scores_no_highways.csv')
+merged_osm = import_data('scores_with_diff_weights.csv')
 
 # Initializing OSM_bike Graph
-@st.cache(allow_output_mutation=True, hash_funcs={gpd.GeoDataFrame: lambda _: None})
+# @st.cache(allow_output_mutation=True, hash_funcs={gpd.GeoDataFrame: lambda _: None})
+@st.cache_resource
 def bike_graph(_edges):
     edges = gpd.GeoDataFrame(_edges).set_crs("epsg:4326")
     G = ox.graph_from_gdfs(nodes, _edges)
@@ -52,6 +54,7 @@ def bike_graph(_edges):
 bike_edges, G_bike = bike_graph(merged_osm)
     
 # Get shortest route from OSM
+@st.cache_resource
 def get_osm_route(start_location, dest_location):
      
     start_data = get_lat_lon(start_location)
@@ -65,17 +68,45 @@ def get_osm_route(start_location, dest_location):
     return shortest_route, pathDistance
 
 # Get best bike route from OSM
-def get_bike_route(graph, start_location, dest_location, weight):
+@st.cache_resource
+def get_bike_route(_graph, start_location, dest_location, weight):
      
     start_data = get_lat_lon(start_location)
     dest_data = get_lat_lon(dest_location)
 
-    orig_edge = ox.nearest_edges(graph, start_data[1], start_data[0])
-    dest_edge = ox.nearest_edges(graph, dest_data[1], dest_data[0])
-    best_route = ox.shortest_path(graph, orig_edge[0], dest_edge[1], weight=weight)
-    # best_pathDistance = nx.shortest_path_length(graph, orig_edge, dest_edge, weight=weight)
+    orig_edge = ox.nearest_edges(_graph, start_data[1], start_data[0])
+    dest_edge = ox.nearest_edges(_graph, dest_data[1], dest_data[0])
+    best_route = ox.shortest_path(_graph, orig_edge[0], dest_edge[1], weight=weight)
+    # Calculate the total length of the path using the 'length' attribute of the edges
+    bike_pathDistance = sum(_graph[u][v][0]['length'] for u, v in zip(best_route[:-1], best_route[1:]))
     
-    return best_route #, best_pathDistance
+    return best_route, bike_pathDistance
+
+# Calculate the midpoint between start & destination
+@st.cache_resource
+def calculate_midpoint(start_data, dest_data):
+    mid_lat = (start_data[0] + dest_data[0]) / 2
+    mid_lon = (start_data[1] + dest_data[1]) / 2
+    return (mid_lat, mid_lon)
+
+# Function to calculate bikeability score
+@st.cache_resource
+def calculate_bikeability_score(_graph, route):
+    scores = []
+    # Iterate over the route to get pairs of nodes (start, end) representing each edge
+    for start, end in zip(route[:-1], route[1:]):
+        try:
+            edge_data = _graph[start][end][0]
+            score = edge_data.get('noelevationScore', None)
+            if score is not None:
+                scores.append(score)
+        except KeyError:
+            # Means we have a motorway, primary road, etc. in the route
+            score = 0.2
+            scores.append(score)
+    mean_score = sum(scores) / len(scores)
+    return mean_score
+
 
    
 # App layout
@@ -97,49 +128,98 @@ route_type = st.selectbox('Select route type:', ['Shortest Route',
 if st.button('Find Route'):
     start_data = get_lat_lon(start_location)
     dest_data = get_lat_lon(dest_location)
+    
+    # Calculate the midpoint
+    midpoint = calculate_midpoint(start_data, dest_data)
 
     # Create folium map
-    m = folium.Map(location=start_data, zoom_start=11)
-
-    # Add markers and polyline
-    folium.Marker(start_data, popup='Start',
-                  icon = folium.Icon(color='green', prefix='fa',icon='bicycle')).add_to(m)
-    folium.Marker(dest_data, popup='Destination', icon = folium.Icon(color='red', icon="flag")).add_to(m)
+    m = folium.Map(location=midpoint, zoom_start=13)
     
+    # Get the best routes
+    bikeable_route, bike_pathDistance = get_bike_route(G_bike, start_location, dest_location, "noelevationScore_reversed")
+    bike_geom = [(G_bike.nodes[node]['y'], G_bike.nodes[node]['x']) for node in bikeable_route]
+    shortest_route, pathDistance = get_osm_route(start_location, dest_location)
+    route_geom = [(G.nodes[node]['y'], G.nodes[node]['x']) for node in shortest_route]
+    
+    # Calculate bikeability score
+    bike_score = calculate_bikeability_score(G_bike, bikeable_route)
+    short_score = calculate_bikeability_score(G_bike, shortest_route)
+
     # Convert route type to lowercase for consistency
     route_type_lower = route_type.lower()
 
     if route_type_lower == 'compare routes':
-        bikeable_route = get_bike_route(G_bike, start_location, dest_location, "weightedFinalScore_reversed")
-        bike_geom = [(G_bike.nodes[node]['y'], G_bike.nodes[node]['x']) for node in bikeable_route]
         folium.PolyLine(bike_geom, color="blue", weight=4, opacity=1).add_to(m)
         folium.Marker(start_data, popup='Start',
                       icon = folium.Icon(color='green', prefix='fa',icon='bicycle')).add_to(m)
         folium.Marker(dest_data, popup='Destination', icon = folium.Icon(color='red', icon="flag")).add_to(m)
 
         # Fetch and display the shortest route
-        shortest_route, pathDistance = get_osm_route(start_location, dest_location)
-        route_geom = [(G.nodes[node]['y'], G.nodes[node]['x']) for node in shortest_route]
         folium.PolyLine(route_geom, color="red", weight=4, opacity=0.5).add_to(m)
     
     elif route_type_lower == 'bike-friendly route':
-        # Get the best bike route
-        bikeable_route = get_bike_route(G_bike, start_location, dest_location, "weightedFinalScore_reversed")
-        
-        m = ox.plot_route_folium(G_bike, bikeable_route, tiles='openstreetmap')
+        folium.PolyLine(bike_geom, color="blue", weight=4, opacity=1).add_to(m)
         folium.Marker(start_data, popup='Start',
                       icon = folium.Icon(color='green', prefix='fa',icon='bicycle')).add_to(m)
         folium.Marker(dest_data, popup='Destination', icon = folium.Icon(color='red', icon="flag")).add_to(m)
     else:      
         # Get the shortest route
-        shortest_route, pathDistance = get_osm_route(start_location, dest_location)
-         
-        m = ox.plot_route_folium(G, shortest_route, tiles='openstreetmap')
+        folium.PolyLine(route_geom, color="blue", weight=4, opacity=1).add_to(m)
         folium.Marker(start_data, popup='Start',
                       icon = folium.Icon(color='green', prefix='fa',icon='bicycle')).add_to(m)
-        popup_text = f'Destination<br><br>Distance: {round(pathDistance/1000, 1)} km'
-        folium.Marker(dest_data, popup=popup_text, icon = folium.Icon(color='red', icon="flag")).add_to(m)
+        folium.Marker(dest_data, popup='Destination', icon = folium.Icon(color='red', icon="flag")).add_to(m)
 
+    
+    # Display route information prominently using Streamlit widgets
+    if route_type_lower == 'bike-friendly route':
+        st.markdown(f"#### Bike-Friendly Route Details")
+        st.write(f"**Distance:** {round(bike_pathDistance/1000, 2)} km")
+        st.write(f"**Estimated Time Needed:** {round((bike_pathDistance/1000 / 15) * 60)} minutes")
+    elif route_type_lower == 'shortest route':
+        st.markdown(f"#### Shortest Route Details")
+        st.write(f"**Distance:** {round(pathDistance/1000, 2)} km")
+        st.write(f"**Estimated Time Needed:** {round((pathDistance/1000 / 15) * 60)} minutes")
+    elif route_type_lower == 'compare routes':
+        col1, col2 = st.columns(2)
+        with col1:
+            st.markdown(f"#### Bike-Friendly Route")
+            st.write(f"**Distance:** {round(bike_pathDistance/1000, 2)} km")
+            st.write(f"**Estimated Time Needed:** {round((bike_pathDistance/1000 / 15) * 60)} minutes")
+            st.write(f"**Average Bikeability Score:** {round(bike_score*100, 2)} %")
+        with col2:
+            st.markdown(f"#### Shortest Route")
+            st.write(f"**Distance:** {round(pathDistance/1000, 2)} km")
+            st.write(f"**Estimated Time Needed:** {round((pathDistance/1000 / 15) * 60)} minutes")
+            st.write(f"**Average Bikeability Score:** {round(short_score*100, 2)} %")
+        
+        st.markdown("""
+        <style>
+        .legend {
+            display: flex;
+            align-items: center;
+        }
+        .color-box {
+            width: 12px;
+            height: 12px;
+            margin-right: 5px;
+            border: 1px solid #888;
+        }
+        .blue-box {
+            background-color: blue;
+        }
+        .red-box {
+            background-color: red;
+        }
+        </style>
+        <div class="legend">
+            <div class="color-box blue-box"></div><span>- Bike-friendly Route</span>
+        </div>
+        <div class="legend">
+            <div class="color-box red-box"></div><span>- Shortest Route</span>
+        </div>
+        """, unsafe_allow_html=True)
+        
+    
     # Display the map
     folium_static(m, width=700)
     
